68872d523e2c1f4880fa31017295e4acee008d18
salonikoo <94130577+salonikoo@users.noreply.github.com>
2023-03-17 05:24:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+
+// Get the canvas element and the 2D context
+var canvas = document.getElementById("avatar-canvas");
+var ctx = canvas.getContext("2d");
+
+// Draw the avatar layers in the correct order
+ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+ctx.drawImage(clothesImage, 0, 0, canvas.width, canvas.height);
+ctx.drawImage(skinImage, 0, 0, canvas.width, canvas.height);
+ctx.drawImage(hairImage, 0, 0, canvas.width, canvas.height);
+ctx.drawImage(accessoriesImage, 0, 0, canvas.width, canvas.height);
+
+// Add event listener to download button
+var downloadBtn = document.getElementById("download-btn");
+downloadBtn.addEventListener("click", function() {
+  // Convert the canvas to a data URL and download the image
+  var dataURL = canvas.toDataURL("image/png");
+  downloadBtn.href = dataURL;
+  downloadBtn.download = "avatar.png";
+});
+
+
 // Get all the avatar component elements
 const avatarFace = document.querySelector('.avatar-face img');
 const avatarHair = document.querySelector('.avatar-hair img');
